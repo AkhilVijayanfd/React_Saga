b9a140cd4363469141bb67a7fba9d2d6337ac1f2
AkhilVijayanfd <92812336+AkhilVijayanfd@users.noreply.github.com>
2022-01-25 09:05:22 +0530

    API Fetch

--- src/App.js
+++ src/App.js
@@ -1,38 +1,27 @@
+
 import React from "react";
+import { useEffect } from "react";
+import {  getUsers } from "./actions";
 import { useSelector, useDispatch } from "react-redux";
-import { getUsersFetch } from './actions/actions';
-import Button from "./components/button";
 
-function App(){
-     const dispatch = useDispatch();
-     const users = useSelector(state => state.myFirstReducer.users);
-    
-     return(
-        <div className="App">
-             <button onClick={() => dispatch(getUsersFetch())}>Enter to View API</button>
-             <table border="2">
-                  <thead>
-                       <tr>
-                            <th>ID</th>
-                            <th>NAME</th>
-                            <th>EMAIL</th>
-                            <th>VIEW</th>
-                       </tr>
-                  </thead>
-                  <tbody>
-                       {users.map(u =>
-                       <tr key={u.id}>
-                            <td>{u.id}</td>
-                            <td>{u.name}</td>
-                            <td>{u.email}</td>
-                            <td><Button/></td>
-                       </tr>
-                         )}
-                  </tbody>
-             </table>
-             
-        </div>
+function App() {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users.users);
+  const loading = useSelector((state) => state.users.loading);
+  const error = useSelector((state) => state.users.error);
 
-     );
+  useEffect(() => {
+    dispatch(getUsers());
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Welcome to React with Redux Saga </h1>
+      {loading && <h2>Loading...</h2>}
+      {error && !loading && <h2>{error}</h2>}
+      {users && users.map((user, i) => <h2 key={i}>{user.name}</h2>)}
+    </div>
+  );
 }
+
 export default App;
